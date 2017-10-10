f34e277f5718009812f7a490f69197fc55529513
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-10 14:32:51 +0100

    built busstop proxy class using restsharp docs, now need to pull class output into main.

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-            Console.WriteLine("HelloWorld!");
-            Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your bus stop code.");
+            var busStop = Console.ReadLine();
+
+        }
     }
-  }
+
+    //proxy class as restsharp api asks
+    public class BusStopProxyAPI
+    {
+        const string BaseUrl = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals";
+
+        readonly string _accountSid;
+        readonly string _secretKey;
+
+        public BusStopProxyAPI(string accountSid, string secretKey)
+        {
+            _accountSid = accountSid;
+            _secretKey = secretKey;
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new System.Uri(BaseUrl);
+            client.Authenticator = new HttpBasicAuthenticator(_accountSid, _secretKey);
+            request.AddParameter("AccountSid", _accountSid, ParameterType.UrlSegment); //
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response. Check inner details for more info.";
+                var twilioException = new ApplicationException(message, response.ErrorException);
+                throw twilioException;
+            }
+            return response.Data;
+        }
+    }
 }
