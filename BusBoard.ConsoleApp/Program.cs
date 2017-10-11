9c0ef1ef6a845e5710254744d0f16d81b3c4c837
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-11 17:50:46 +0100

    Completely restructured and changed order to function focused,.

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -12,19 +12,44 @@ namespace BusBoard.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string AskUserForPostcode()
         {
-            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            Console.WriteLine("Please enter your postcode.");
+            string postCode = Console.ReadLine();
+            return postCode;
+        }
 
-            Console.WriteLine("Please enter your bus stop code.");
-            var busStop = Console.ReadLine();
+        private static PostcodeLocation GetPostcodeLocationFromPostcodeAPI(string postcode)
+        {
+            var PCclient = new RestClient("https://api.postcodes.io");
+            var PCrequest = new RestRequest("/postcodes/" + postcode + "", Method.GET);
+            var response = PCclient.Execute<PostCodeFields>(PCrequest);
+            return response.Data.result;
+        }
 
-            BusStopAPI(busStop, out RestClient client, out RestRequest request);
-            IRestResponse response = client.Execute(request);
-
-            IEnumerable<ArrivalPrediction> limitedArrivals = OrderedByArrivals(response);
-
-            foreach (var Arrival in limitedArrivals)
+        private static string GetBusStopCodeForLocationFromTFL(float lat,  float lon)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk");
+            client.Authenticator = new HttpBasicAuthenticator("6d4298b1", "63f65d0071453ee130f132a483601da1");
+            var request = new RestRequest("StopPoint?stopTypes=NaptanPublicBusCoachTram&returnLines=true&lat=" + lat + "&lon=" + lon + "", Method.GET);
+            request.AddParameter("app_id", "6d4298b1");
+            request.AddParameter("app_key", "63f65d0071453ee130f132a483601da1");
+            var response = client.Execute<StopPointDetails>(request);
+            return response.Data.stopPoints.First().naptanId;
+        }
+        private static IEnumerable<ArrivalPrediction> GetBusStopArrivalTimesFromTFL(string busStop)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk");
+            client.Authenticator = new HttpBasicAuthenticator("6d4298b1", "63f65d0071453ee130f132a483601da1");
+            var request = new RestRequest("StopPoint/" + busStop + "/Arrivals", Method.GET);
+            var response = client.Execute<List<ArrivalPrediction>>(request);
+            var arrivals = response.Data;
+            var OrderedArrivals = arrivals.OrderBy(arrival => arrival.TimeToStation).Take(5);
+            return OrderedArrivals;
+        }
+        private static void PrintArrivalTimesToConsole(IEnumerable<ArrivalPrediction> arrivalTimes)
+        {
+            foreach (var Arrival in arrivalTimes)
             {
                 Console.WriteLine(Arrival.Id);
                 Console.WriteLine(Arrival.StationName);
@@ -33,19 +58,62 @@ namespace BusBoard.ConsoleApp
             Console.ReadLine();
         }
 
+        static void Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            var postcode = AskUserForPostcode();
+            var postcodeLocation = GetPostcodeLocationFromPostcodeAPI(postcode);
+            var busStopCode = GetBusStopCodeForLocationFromTFL(postcodeLocation.latitude, postcodeLocation.longitude);
+            var arrivalTimes = GetBusStopArrivalTimesFromTFL(busStopCode);
+            PrintArrivalTimesToConsole(arrivalTimes);
+
+
+            //Console.WriteLine("Please enter your bus stop code.");
+            //string busStop = Console.ReadLine();
+
+            //Console.WriteLine("Please enter your postcode.");
+            //string postCode = Console.ReadLine();
+
+            //BusStopAPI(busStop, out RestClient client, out RestRequest request);
+
+            //PostCodeAPI(postCode, out RestClient PCclient, out RestRequest PCrequest);
+
+            //IRestResponse<PostCodeFields> response = client.Execute<PostCodeFields>(request);
+            //var postcodeLocation = response.Data;
+
+            //IEnumerable<ArrivalPrediction> limitedArrivals = OrderedByArrivals(response);
+
+            //foreach (var Arrival in limitedArrivals)
+            //{
+            //    Console.WriteLine(Arrival.Id);
+            //    Console.WriteLine(Arrival.StationName);
+            //    Console.WriteLine("This bus is due in {0}", Arrival.GetArrivalTime());
+            //}
+            //Console.ReadLine();
+        }
+
         private static IEnumerable<ArrivalPrediction> OrderedByArrivals(IRestResponse response)
         {
             List<ArrivalPrediction> ArrivalPredictions = JsonConvert.DeserializeObject<List<ArrivalPrediction>>(response.Content);
             IEnumerable<ArrivalPrediction> orderedArrivals = ArrivalPredictions.OrderBy(ap => ap.TimeToStation); //lambda expression.
-            var limitedArrivals = orderedArrivals.Take(5);
-            return limitedArrivals;
+            var limitedArrivals = orderedArrivals.Take(5);//Take(n) returns the specified amount of objects from the list.
+            return limitedArrivals; //returns the limited arrivals Enum to the main function, as method is not void 
         }
 
-        private static void BusStopAPI(string busStop, out RestClient client, out RestRequest request)
+        private static void BusStopAPI(string busStop, string postcodeLocation, out RestClient client, out RestRequest request)
         {
             client = new RestClient("https://api.tfl.gov.uk");
-            request = new RestRequest("StopPoint/" + busStop + "/Arrivals", Method.GET);
+            request = new RestRequest("StopPoint/" + busStop + "/StopPoint_GetByGeoPoint", Method.GET);
+            request = new RestRequest("StopPoint?stopTypes=NaptanPublicBusCoachTram&returnLines=true&lat=" + postcodeLocation + "&lon=" + postcodeLocation + "", Method.GET);
+
             client.Authenticator = new HttpBasicAuthenticator("6d4298b1", "63f65d0071453ee130f132a483601da1");
+        }
+
+        private static void PostCodeAPI(string postCode, out RestClient PCclient, out RestRequest PCrequest)
+        {
+            PCclient = new RestClient("https://api.postcodes.io");
+            PCrequest = new RestRequest("/postcodes/" + postCode + "", Method.GET);
         }
     }
 }
@@ -59,14 +127,31 @@ public class ArrivalPrediction
     public string GetArrivalTime()
     {
         TimeSpan time = TimeSpan.FromSeconds(TimeToStation);
-        String stringTime = time.ToString(@"hh\:mm\:ss");
+        String stringTime = time.ToString(@"\:mm\:ss");
         return stringTime;
     }
 }
 
-    
 
+public class PostCodeFields
+{
+    public string status { get; set; }
+    public PostcodeLocation result { get; set; }
+}
+public class PostcodeLocation
+{
+    public float longitude { get; set; }
+    public float latitude { get; set; }
+}
 
-
+public class StopPointDetails
+{
+    public List<float> centrePoint { get; set; }
+    public List<SingleStopPoint> stopPoints { get; set; }
+}
+public class SingleStopPoint
+{
+    public string naptanId { get; set; }
+}
 
 
