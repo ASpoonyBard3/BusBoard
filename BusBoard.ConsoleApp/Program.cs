32c971a0658bb894930b7b92b51ba77f7298d91b
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-11 12:09:13 +0100

    Lambdas makes my head hurty.

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -18,77 +18,37 @@ namespace BusBoard.ConsoleApp
 
             Console.WriteLine("Please enter your bus stop code.");
             var busStop = Console.ReadLine();
-            //
-            BusStopProxyAPI busStopAPI = new BusStopProxyAPI("6d4298b1", "63f65d0071453ee130f132a483601da1");
 
-            var client = new RestClient("https://api.tfl.gov.uk");
-            var request = new RestRequest("StopPoint/"+ busStop + "/Arrivals", Method.GET);
-
-            client.Authenticator = new HttpBasicAuthenticator("6d4298b1", "63f65d0071453ee130f132a483601da1");
-            //var request = new RestResponse();
-
+            BusStopAPI(busStop, out RestClient client, out RestRequest request);
             IRestResponse response = client.Execute(request);
 
-            List<ArrivalPrediction> ArrivalPredictions = JsonConvert.DeserializeObject<List<ArrivalPrediction>>(response.Content);
-            IEnumerable<ArrivalPrediction> orderedArrivals = ArrivalPredictions.OrderBy(ap => ap.TimeToStation);
-            var limitedArrivals = orderedArrivals.Take(5);
+            IEnumerable<ArrivalPrediction> limitedArrivals = OrderedByArrivals(response);
 
             foreach (var Arrival in limitedArrivals)
             {
                 Console.WriteLine(Arrival.Id);
                 Console.WriteLine(Arrival.StationName);
-                Console.WriteLine(Arrival.GetArrivalTime());
+                Console.WriteLine("This bus is due in {0}", Arrival.GetArrivalTime());
             }
             Console.ReadLine();
         }
-    }
-}
-/*
-public static Task<IRestResponse> GetRestResponseContentAsync(RestClient theClient, RestRequest theRequest)
-{
-    var tcs = new TaskCompletionSource<IRestResponse>();
-    theClient.ExecuteAsync(theRequest, theResponse =>
-    {
-        tcs.SetResult(theResponse);
-    });
-    return tcs.Task;
-}
-*/
 
-//proxy class as restsharp api asks
-public class BusStopProxyAPI
-{
-    const string BaseUrl = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals";
-
-    private readonly string _accountSid;
-    private readonly string _secretKey;
-
-    public BusStopProxyAPI(string appID, string appKey)
-    {
-        _accountSid = appID;
-        _secretKey = appKey;
-    }
-
-    public T Execute<T>(RestRequest request) where T : new()
-    {
-        var client = new RestClient
+        private static IEnumerable<ArrivalPrediction> OrderedByArrivals(IRestResponse response)
         {
-            BaseUrl = new System.Uri(BaseUrl),
-            Authenticator = new HttpBasicAuthenticator(_accountSid, _secretKey)
-        };
-        request.AddParameter("AccountSid", _accountSid, ParameterType.UrlSegment); //
-        var response = client.Execute<T>(request);
-
-        if (response.ErrorException != null)
-        {
-            const string message = "Error retrieving response. Check inner details for more info.";
-            var exception = new ApplicationException(message, response.ErrorException);
-            throw exception;
+            List<ArrivalPrediction> ArrivalPredictions = JsonConvert.DeserializeObject<List<ArrivalPrediction>>(response.Content);
+            IEnumerable<ArrivalPrediction> orderedArrivals = ArrivalPredictions.OrderBy(ap => ap.TimeToStation); //lambda expression.
+            var limitedArrivals = orderedArrivals.Take(5);
+            return limitedArrivals;
         }
-        return response.Data;
+
+        private static void BusStopAPI(string busStop, out RestClient client, out RestRequest request)
+        {
+            client = new RestClient("https://api.tfl.gov.uk");
+            request = new RestRequest("StopPoint/" + busStop + "/Arrivals", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("6d4298b1", "63f65d0071453ee130f132a483601da1");
+        }
     }
 }
-
 
 public class ArrivalPrediction
 {
